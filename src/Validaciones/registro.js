1132c61fbfbe6134190fba49fe09bbaa4f6f10b8
Javier348solis <javiersolisretana@gmail.com>
2024-07-24 10:06:21 -0600

    validacion registro

--- src/Validaciones/registro.js
+++ src/Validaciones/registro.js
@@ -7,17 +7,24 @@ let usuarioRegistro = JSON.parse(localStorage.getItem("usuarioRegistro")) || []
 
 boton.addEventListener("click", function (e){
     e.preventDefault()
+    if (nombre.value === "" || correo.value==="" || contrasena.value==="") {
+        alert("Los campos estan vacios")
+    } else {
+        alert(`Hola ${nombre.value} `)
+    }
      let usuario = {
         nombre: nombre.value,
         correo: correo.value,
         contra: contrasena.value,
      }
+
      let existe = usuarioRegistro.find(user => nombre === user.nombre || correo === user.correo);
             if (existe) {
                 alert("El usuario ya existe")
             }else{
                 usuarioRegistro.push(usuario)
                 localStorage.setItem("usuarioRegistro", JSON.stringify(usuarioRegistro))
+                window.location.href
             }   
         }
 ) 
