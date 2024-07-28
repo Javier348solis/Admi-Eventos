81b9bfb011f101e9f398b08bc377efbe476e672d
Javier348solis <javiersolisretana@gmail.com>
2024-07-28 00:42:17 -0600

    creacion pagina secundaria

--- src/Validaciones/index.js
+++ src/Validaciones/index.js
@@ -6,10 +6,12 @@ const agregar = document.getElementById("espaciosTareas1")
 const fecha = document.getElementById("espaciosTareas2")
 const hora = document.getElementById("espaciosTareas3")
 const botoncito = document.getElementById("botonAgregar")
+const contenedor = document.getElementById("input-salida")
 let isOpen =false;
 
 let agregarTareas = JSON.parse(localStorage.getItem("agregarTareas")) || []
 
+//Validacion de los espacios vacios de los inputs que agregaran las tareas o eventos
 botoncito.addEventListener("click", ()=>{
     if (agregar.value ==="" || fecha.value === "" || hora.value === "") {
         alert("No se pueden agregar tareas o eventos vacios")
@@ -17,12 +19,17 @@ botoncito.addEventListener("click", ()=>{
         alert("Se agrego satisfactoriamente")
     }
 })
+//Se define un objeto, para luego darle el valor a los inputs que se necesitan
 let espaciosTareas = {
     agregar: agregar.value,
     fecha: fecha.value,
     hora: hora.value,
 }
-
+contenedor.innerHTML = `
+        <p><strong>Tarea o Evento:</strong> ${agregar}</p>
+         <p><strong>Fecha:</strong> ${fecha}</p>
+          <p><strong>Hora:</strong> ${hora}</p>
+`
 
 //Funcion que se le dio al boton del navbar, si esta abierto, se mantiene verdadero, si esta cerrado, falso.
 boton.addEventListener("click", ()=> {
